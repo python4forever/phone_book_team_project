57d680b853bd3bd8d5865fd7c23fb8620d9e4a9f
AndreiParmon <parmonandrei@gmail.com>
2023-03-19 19:16:34 +0300

    added version 2.0 of record file

--- record.py
+++ record.py
@@ -1,25 +1,32 @@
+import csv
+
 last_id = 0
 all_data = []
 file_base = 'tel_book.csv'
 
 
-def record(name, patronymic, surname, phone, comment):
+def record(name='', patronymic='', surname='', phone='', comment=''):
     global last_id
     global all_data
 
 
+    name = input('Введите имя')
     if(name == ''):
         print('Поле не заполнено!')
         return
+    patronymic = input('Введите отчество')
     if(patronymic == ''):
         print('Поле не заполнено!')
         return
+    surname = input('Введите фамилию')
     if(surname == ''):
         print('Поле не заполнено!')
         return
+    phone = input('Введите номер телефона')
     if(phone == ''):
         print('Поле не заполнено!')
         return
+    comment = input('Введите комментарий')
     if(comment == ''):
         print('Поле не заполнено!')
         return
@@ -37,5 +44,7 @@ def record(name, patronymic, surname, phone, comment):
     new_all_data = [str(last_id), name.title(), patronymic.title(), surname.title(), phone, comment.title()]
     all_data.append(new_all_data)
 
-    with open('tel_book.csv', 'a') as file:
-        file.write('record;\n')
+    with open('tel_book.csv', 'a', encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=';', quotechar='\'', 
+                            quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(new_all_data)
