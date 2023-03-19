6e40269f02a9c35bac0de02ef6f314d928e49068
Gaiah <gaiah.a.akopian@gmail.com>
2023-03-20 03:17:06 +0400

    some commit

--- record.py
+++ record.py
@@ -1,6 +1,9 @@
+def format_data(all_data, sep):
+    separated_data = ''
+    for i in all_data:
+        separated_data += sep.join(i.values())
+    return separated_data
 
-
-def format_data(all_data, src):
-    with open(src, 'a', encoding="utf-8") as file:
-        writer = csv.writer(file, delimiter=';', quotechar='\'', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(all_data)
+def write_data(all_data, src):
+    with open(src, 'w', encoding='utf-8') as file:
+        file.write(format_data(all_data), '||')
